c2d81aa0845ce260d4d9f78dc55b56ecb7c54a81
Gaurav Naik <gvrvnk@gmail.com>
2017-02-20 13:17:14 +0530

    [Fix] Updated links on module page for Awfis ERPNext

--- awfis_erpnext/config/awfis_erpnext.py
+++ awfis_erpnext/config/awfis_erpnext.py
@@ -3,18 +3,6 @@ from frappe import _
 def get_data():
 	return [
 		{
-			"label": _("Settings"),
-			"icon": "icon-star",
-			"items": [
-				{
-					"type": "doctype",
-					"name": "Awfis Settings",
-					"label": "Awfis Settings",
-					"description": _("General settings for Awfis ERPNext"),
-				},
-			]
-		},
-		{
 			"label": _("CRM"),
 			"icon": "icon-star",
 			"items": [
@@ -29,7 +17,31 @@ def get_data():
 					"name": "awfis_lead_list",
 					"label": "Lead Dashboard",
 					"description": _("Dashboard with actionable grids for Lead."),
+				},
+				{
+					"type": "doctype",
+					"name": "Awfis Centre",
+					"label": "Awfis Centre",
+					"description": _("Awfis Centre"),
+				},
+				{
+					"type": "doctype",
+					"name": "Awfis Guest",
+					"label": "Awfis Guest",
+					"description": _("Awfis Guest"),
 				},	
+			]
+		},
+		{
+			"label": _("Settings"),
+			"icon": "icon-star",
+			"items": [
+				{
+					"type": "doctype",
+					"name": "Awfis Settings",
+					"label": "Awfis Settings",
+					"description": _("General settings for Awfis ERPNext"),
+				},
 			]
 		},
 		{
@@ -41,6 +53,20 @@ def get_data():
 					"name": "Awfis Report Processing Tool",
 					"label": "Awfis Report Processing Tool",
 					"description": _("Awfis Report Processing Tool"),
+				},
+			]
+		},
+		{		
+			"label": _("MMS Reports (Awfis Material Management System)"),
+			"icon": "icon-star",
+			"items": [
+				{
+					"type": "report",
+					"name": "Item Batch Expiry Status",
+					"label": "Item Batch Expiry Status",
+					"description": _("Report indicating expiry date for Item Batches"),
+					"route": "query-report/Item Batch Expiry Status",
+					"doctype": "DocType"
 				},
 			]
 		},
