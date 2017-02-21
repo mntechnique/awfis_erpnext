82fcac66ab7c2a9a979acd77e2b610812adbbb61
Gaurav Naik <gvrvnk@gmail.com>
2017-02-22 04:39:06 +0530

    [Mod] Lead dash data queries updated as per specs

--- awfis_erpnext/awfis_erpnext/api.py
+++ awfis_erpnext/awfis_erpnext/api.py
@@ -37,20 +37,21 @@ def cleanup(fname):
 @frappe.whitelist()
 def get_lead_list_data(limit=20):
 	user = frappe.get_doc("User", frappe.session.user)
+	
 	assigned_to_user = frappe.get_all("ToDo", filters={"owner": frappe.session.user, "reference_type": "Lead"}, fields=["reference_name"])
-
 	assigned_to_user = [l.get("reference_name") for l in assigned_to_user]	
 
-	follow_up_today, assigned_to_me_open, other = [], [], []
-
 	roles_by_user = [u.role for u in user.user_roles]
+	
+	follow_up_today, assigned_to_me_open, other = [], [], []
 	
 	if "Administrator" in roles_by_user or "System Manager" in roles_by_user:
 		follow_up_today = frappe.db.sql("SELECT * FROM tabLead WHERE date(contact_date) = curdate() ORDER BY name DESC LIMIT {limit}".format(limit=limit), as_dict=True)
 
-		assigned_to_me_open = frappe.get_all("Lead", filters=[["name", "in", assigned_to_user], ["status", "=", "Open"]], fields=["*"], limit=limit)
+		assigned_to_me_open = frappe.get_all("Lead", 
+								filters=[["name", "in", assigned_to_user], ["status", "=", "Open"], ["_comments", "=", None]], fields=["*"], limit=limit)
 	
-		other = frappe.get_all("Lead", fields=["*"], order_by="lead_state, contact_date DESC", limit=limit)
+		other = frappe.get_all("Lead", filters=[["name", "in", assigned_to_user]], order_by="lead_state, contact_date DESC", limit=limit)
 
 	elif "Sales Manager" in roles_by_user or "Sales User" in roles_by_user or "Awfis Ops User" in roles_by_user:
 		allowed_territories = frappe.get_all("DefaultValue", fields=["defvalue"], filters={"defkey": "Territory", "parenttype": "User Permission", "parent":frappe.session.user})
@@ -64,8 +65,8 @@ def get_lead_list_data(limit=20):
 												territories= ",".join(["'" + at + "'" for at in allowed_territories_list]) 
 											), as_dict=True)
 		
-		assigned_to_me_open = frappe.get_all("Lead", filters=[["name", "in", assigned_to_user], ["status", "=", "Open"]], fields=["*"], limit=limit)
-		other = frappe.get_all("Lead", filters=[["awfis_lead_territory", "in", allowed_territories_list]], fields=["*"], order_by="lead_state, contact_date DESC", limit=limit)
+		assigned_to_me_open = frappe.get_all("Lead", filters=[["name", "in", assigned_to_user], ["status", "=", "Open"], ["_comments", "=", None]], fields=["*"], limit=limit)
+		other = frappe.get_all("Lead", filters=[["awfis_lead_territory", "in", allowed_territories_list], ["name", "in", assigned_to_user]], fields=["*"], order_by="lead_state, contact_date DESC", limit=limit)
 	out = frappe._dict({
 		"follow_up_today": follow_up_today,
 		"assigned_to_me_open": assigned_to_me_open,
