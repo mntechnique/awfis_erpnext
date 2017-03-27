5c1740bfec06fa316f810b607dbd18241a1e5927
Gaurav Naik <gvrvnk@gmail.com>
2017-03-28 03:33:57 +0530

    [Fix] Edits to Lead Dashboard

--- awfis_erpnext/awfis_erpnext/api.py
+++ awfis_erpnext/awfis_erpnext/api.py
@@ -2,7 +2,7 @@ import frappe
 from frappe import _
 import os
 import io
-
+import ast
 
 def get_filedata(file_name, options=None):
 	fname = file_name #os.path.join("/tmp", file_name)
@@ -34,6 +34,111 @@ def cleanup(fname):
 	if os.path.exists(fname):
 		os.remove(fname)
 
+
+@frappe.whitelist()
+def get_lead_list_data():
+	all_leads = frappe.get_all("Lead", fields=["*"])
+
+	user = frappe.get_doc("User", frappe.session.user)
+	user_roles = [u.role for u in user.user_roles]
+
+	follow_up_today, open_leads, other_leads = [], [], []
+
+	user_territories = frappe.get_all("DefaultValue", 
+				fields=["defvalue"],
+				filters={"defkey": "Territory", "parenttype": "User Permission", "parent":frappe.session.user}
+				)
+	user_territories = [ut.defvalue for ut in user_territories]
+	
+	if "Sales Manager" in user_roles or "Awfis Ops Manager" in user_roles:
+		team = frappe.get_all("DefaultValue", 
+					fields=["parent"], 
+					filters=[["defkey", "=", "Territory"],["defvalue", "in", user_territories], ["parenttype", "=", "User Permission"]],
+					distinct=True)
+		team = [tm.parent for tm in team]
+
+		print "TEAM", team
+		
+		follow_up_today = [l for l in all_leads if
+							(l.contact_date and frappe.utils.getdate(l.contact_date) == frappe.utils.getdate()) and
+							(
+								(l.owner in team) or 
+								(l._assign and len([assignee for assignee in ast.literal_eval(l._assign) if assignee in team]) > 0)
+							)
+						]
+
+		open_leads = [l for l in all_leads if 
+							(l.owner in team) or 
+							(	
+								(l.contact_by == "") and  
+								(
+									(l._assign and len(ast.literal_eval(l._assign)) == 1) and 
+									("Sales Manager" in [ur.role for ur in frappe.get_doc("User", ast.literal_eval(l._assign)[0]).user_roles])
+								)
+							)
+					]
+		other_leads = [l for l in all_leads if 
+							not (l.contact_date and frappe.utils.getdate(l.contact_date) == frappe.utils.getdate()) and
+							not (	
+								(l.contact_by == "") and  
+								(
+									(l._assign and len(ast.literal_eval(l._assign)) == 1) and 
+									("Sales Manager" in [ur.role for ur in frappe.get_doc("User", ast.literal_eval(l._assign)[0]).user_roles])
+								)
+							) and
+							(
+								(l.owner in team) or 
+								(l._assign and len([assignee for assignee in ast.literal_eval(l._assign) if assignee in team]) > 0)
+							)
+						]
+		#other_leads.sort(key=lambda l: (l.contact_date))
+	elif "Sales User" in user_roles or "Awfis Ops User" in user_roles:
+		follow_up_today = [l for l in all_leads if
+							(l.contact_date and frappe.utils.getdate(l.contact_date) == frappe.utils.getdate()) and
+							(
+								(l.owner == frappe.session.user) or 
+								(frappe.session.user in ast.literal_eval(l._assign))
+							)
+						]
+
+		open_leads = [l for l in all_leads if 
+							(l.owner == frappe.session.user) or 
+							(	
+								(l.contact_by == "") and  
+								(
+									(l._assign and len(ast.literal_eval(l._assign)) == 1) and 
+									("Sales Manager" in [ur.role for ur in frappe.get_doc("User", ast.literal_eval(l._assign)[0]).user_roles])
+								)
+							)
+					]
+		other_leads = [l for l in all_leads if 
+							(
+								(l.contact_date == "")
+								or 
+								(l.contact_date and (frappe.utils.getdate(l.contact_date) != frappe.utils.getdate()))
+							) and
+							(
+								(l.contact_by == "")
+								or 
+								(l.contact_by and not ((
+									(l._assign and len(ast.literal_eval(l._assign)) == 1) and 
+									("Sales Manager" in [ur.role for ur in frappe.get_doc("User", ast.literal_eval(l._assign)[0]).user_roles])
+								)))
+							)
+							and
+							(
+								(l.owner == "") or 
+								(l._assign and len([assignee for assignee in ast.literal_eval(l._assign) if assignee in team]) > 0)
+							)
+						]
+
+		other_leads.sort(key=lambda l: (l.contact_date))
+
+
+	return {"follow_up_today": follow_up_today, "open_leads":open_leads, "other_leads": other_leads}
+	
+
+#ORIGINAL
 # @frappe.whitelist()
 # def get_lead_list_data(limit=20):
 # 	user = frappe.get_doc("User", frappe.session.user)
@@ -82,66 +187,166 @@ def cleanup(fname):
 
 # 	return out
 
-@frappe.whitelist()
-def get_lead_list_data(limit=20):
-	user = frappe.get_doc("User", frappe.session.user)
+#VIABLE
+# @frappe.whitelist()
+# def get_lead_list_data(limit=100):
+# 	user = frappe.get_doc("User", frappe.session.user)
 	
-	leads_assigned_to_user = frappe.get_all("ToDo", filters={"owner": frappe.session.user, "reference_type": "Lead"}, fields=["reference_name"])
-	leads_assigned_to_user = [l.get("reference_name") for l in leads_assigned_to_user]	
+# 	leads_assigned_to_user = frappe.get_all("ToDo", filters={"owner": frappe.session.user, "reference_type": "Lead"}, fields=["reference_name"])
+# 	leads_assigned_to_user = [l.get("reference_name") for l in leads_assigned_to_user]	
 
-	roles_by_user = [u.role for u in user.user_roles]
+# 	roles_by_user = [u.role for u in user.user_roles]
 	
-	follow_up_today, assigned_to_me_open, other = [], [], []
+# 	follow_up_today, assigned_to_me_open, other = [], [], []
 
-	if "Administrator" in roles_by_user or "System Manager" in roles_by_user:
-		follow_up_today = frappe.db.sql("SELECT * FROM tabLead WHERE date(contact_date) = curdate() ORDER BY name DESC LIMIT {limit}".format(limit=limit), as_dict=True)
+# 	if "Administrator" in roles_by_user or "System Manager" in roles_by_user:
+# 		follow_up_today = frappe.db.sql("SELECT * FROM tabLead WHERE date(contact_date) = curdate() ORDER BY name DESC LIMIT {limit}".format(limit=limit), as_dict=True)
 
-		assigned_to_me_open = frappe.get_all("Lead", 
-								filters=[["name", "in", leads_assigned_to_user], ["status", "=", "Open"], ["_comments", "=", None]], fields=["*"], limit=limit)
+# 		assigned_to_me_open = frappe.get_all("Lead", 
+# 								filters=[["name", "in", leads_assigned_to_user], ["status", "=", "Open"], ["_comments", "=", None]], fields=["*"], limit=limit)
 	
-		#other = frappe.get_all("Lead", filters=[["name", "in", leads_assigned_to_user]], order_by="lead_state, contact_date DESC", limit=limit)
-		other = frappe.get_all("Lead", fields=["*"], order_by="lead_state, contact_date DESC", limit=limit)
+# 		#other = frappe.get_all("Lead", filters=[["name", "in", leads_assigned_to_user]], order_by="lead_state, contact_date DESC", limit=limit)
+# 		other = frappe.get_all("Lead", fields=["*"], order_by="lead_state, contact_date DESC", limit=limit)
 
-	else:
-		allowed_territories = frappe.get_all("DefaultValue", fields=["defvalue"], filters={"defkey": "Territory", "parenttype": "User Permission", "parent":frappe.session.user})
-		allowed_territories_list = [at["defvalue"] for at in allowed_territories]
+# 	else:
+# 		allowed_territories = frappe.get_all("DefaultValue", fields=["defvalue"], filters={"defkey": "Territory", "parenttype": "User Permission", "parent":frappe.session.user})
+# 		allowed_territories_list = [at["defvalue"] for at in allowed_territories]
 
-		territories_clause = ""
-		if len(allowed_territories_list):
-			territories_string = ",".join(["'" + at + "'" for at in allowed_territories_list])
-			territories_clause = " AND awfis_lead_territory in ({0}) ".format(territories_string)
+# 		territories_clause = ""
+# 		if len(allowed_territories_list):
+# 			territories_string = ",".join(["'" + at + "'" for at in allowed_territories_list])
+# 			territories_clause = " AND awfis_lead_territory in ({0}) ".format(territories_string)
 
-		if "Sales Manager" in roles_by_user or "Awfis Ops Manager" in roles_by_user:
-			#Will fetch Leads for all people in territories.
-			follow_up_today = frappe.db.sql("""SELECT * FROM tabLead WHERE date(contact_date) = curdate()
-												{territories_clause} ORDER BY name DESC LIMIT {limit}"""
-												.format(
-													limit=limit,
-													territories_clause= territories_clause
-												), as_dict=True)
+# 		if "Sales Manager" in roles_by_user or "Awfis Ops Manager" in roles_by_user:
+# 			#Will fetch Leads for all people in territories.
+# 			follow_up_today = frappe.db.sql("""SELECT * FROM tabLead WHERE date(contact_date) = curdate()
+# 												{territories_clause} ORDER BY name DESC LIMIT {limit}"""
+# 												.format(
+# 													limit=limit,
+# 													territories_clause= territories_clause
+# 												), as_dict=True)
 			
-			assigned_to_me_open = frappe.get_all("Lead", filters=[["name", "in", leads_assigned_to_user], ["status", "=", "Open"]], fields=["*"], limit=limit)
-			other = frappe.get_all("Lead", filters=[["awfis_lead_territory", "in", allowed_territories_list], ["name", "in", leads_assigned_to_user]], fields=["*"], order_by="lead_state, contact_date DESC", limit=limit)
+# 			assigned_to_me_open = frappe.get_all("Lead", filters=[["name", "in", leads_assigned_to_user], ["status", "=", "Open"]], fields=["*"], limit=limit)
+# 			other = frappe.get_all("Lead", filters=[["awfis_lead_territory", "in", allowed_territories_list], ["name", "in", leads_assigned_to_user]], fields=["*"], order_by="lead_state, contact_date DESC", limit=limit)
 		
-		elif "Sales User" in roles_by_user or "Awfis Ops User" in roles_by_user:
-			follow_up_today = frappe.db.sql("""SELECT * FROM tabLead WHERE date(contact_date) = curdate() and owner = '{owner}'
-												 {territories_clause} ORDER BY name DESC LIMIT {limit}"""
-												.format(
-													limit=limit,
-													owner=frappe.session.user,
-													territories_clause= territories_clause
-												), as_dict=True)
+# 		elif "Sales User" in roles_by_user or "Awfis Ops User" in roles_by_user:
+# 			follow_up_today = frappe.db.sql("""SELECT * FROM tabLead WHERE date(contact_date) = curdate() and owner = '{owner}'
+# 												 {territories_clause} ORDER BY name DESC LIMIT {limit}"""
+# 												.format(
+# 													limit=limit,
+# 													owner=frappe.session.user,
+# 													territories_clause= territories_clause
+# 												), as_dict=True)
 			
-			assigned_to_me_open = frappe.get_all("Lead", filters=[["name", "in", leads_assigned_to_user], ["status", "=", "Open"]], fields=["*"], limit=limit)
-			other = frappe.get_all("Lead", filters=[["awfis_lead_territory", "in", allowed_territories_list], ["name", "in", leads_assigned_to_user]], fields=["*"], order_by="lead_state, contact_date DESC", limit=limit)
+# 			assigned_to_me_open = frappe.get_all("Lead", filters=[["name", "in", leads_assigned_to_user], ["status", "=", "Open"]], fields=["*"], limit=limit)
+# 			other = frappe.get_all("Lead", filters=[["awfis_lead_territory", "in", allowed_territories_list], ["name", "in", leads_assigned_to_user]], fields=["*"], order_by="lead_state, contact_date DESC", limit=limit)
 
-	out = frappe._dict({
-		"follow_up_today": follow_up_today,
-		"assigned_to_me_open": assigned_to_me_open,
-		"other": other
-	})
+# 	out = frappe._dict({
+# 		"follow_up_today": follow_up_today,
+# 		"assigned_to_me_open": assigned_to_me_open,
+# 		"other": other
+# 	})
 
-	return out
+# 	return out
+
+
+#INVIABLE
+# @frappe.whitelist()
+# def get_lead_list_data(limit=0):
+# 	import ast
+
+
+# 	#Preload essentials
+# 	user = frappe.get_doc("User", frappe.session.user)
+# 	roles_by_user = [u.role for u in user.user_roles]
+
+# 	follow_up_today, open_general, other = [], [], []
+
+# 	#Administrators and system managers get unrestricted view of leads across territories. 
+# 	if "Administrator" in roles_by_user or "System Manager" in roles_by_user:
+# 		open_leads_prelim = frappe.get_all("Lead", filters=[["contact_by", "=", ""]], fields=["*"])
+# 		open_leads = [ol for ol in open_leads_prelim if not(ol._assign) or len(ast.literal_eval(ol._assign)) == 1]
+			
+# 		follow_up_today = frappe.db.sql("SELECT * FROM tabLead WHERE date(contact_date) = curdate() ORDER BY name DESC LIMIT {limit}".format(limit=limit), as_dict=True)
+# 		open_general = open_leads
+# 		other = frappe.get_all("Lead", filters=[["contact_by", "!=", ""]], fields=["*"], order_by="lead_state, contact_date DESC", limit=limit)
+	
+# 	else:
+# 		allowed_territories_list = frappe.get_all("DefaultValue", fields=["defvalue"], filters={"defkey": "Territory", "parenttype": "User Permission", "parent":frappe.session.user})
+# 		allowed_territories = [at["defvalue"] for at in allowed_territories_list]
+
+# 		territories_clause, territories_string = "", ""
+		
+# 		if len(allowed_territories_list) > 0:
+# 			territories_string = ",".join(["'" + at + "'" for at in allowed_territories])
+# 			territories_clause = " AND awfis_lead_territory in ({0}) ".format(territories_string)
+
+
+# 		if "Sales Manager" in roles_by_user or "Awfis Ops Manager" in roles_by_user:
+# 			follow_up_today = frappe.db.sql("""SELECT * FROM tabLead WHERE date(contact_date) = curdate() 
+# 					{territories_clause} ORDER BY name DESC LIMIT {limit}"""
+# 					.format(limit=limit,territories_clause= territories_clause), as_dict=True)
+
+# 			#Get Open Leads from allowed territories						
+# 			open_leads_prelim = frappe.get_all("Lead", filters=[
+# 					["contact_by", "=", ""], 
+# 					["awfis_lead_territory", "in", allowed_territories]
+# 					], fields=["*"])
+# 			open_leads = [ol for ol in open_leads_prelim if not(ol._assign) or len(ast.literal_eval(ol._assign)) == 1]
+# 			open_general = open_leads
+
+# 			other = frappe.get_all("Lead", filters=[
+# 						["awfis_lead_territory", "in", allowed_territories]
+# 						["contact_by", "!=", ""]
+# 					], fields=["*"], order_by="lead_state, contact_date DESC", limit=limit)
+
+# 		elif "Sales User" in roles_by_user or "Awfis Ops User" in roles_by_user:
+# 			# leads_assigned_to_user = frappe.get_all("ToDo", filters={"owner": frappe.session.user, "reference_type": "Lead"}, fields=["reference_name"])
+# 			# leads_assigned_to_user = [l.get("reference_name") for l in leads_assigned_to_user]	
+# 			follow_up_today = frappe.db.sql("""SELECT * FROM tabLead WHERE date(contact_date) = curdate() and owner = '{owner}'
+# 												 {territories_clause} ORDER BY name DESC LIMIT {limit}"""
+# 												.format(
+# 													limit=limit,
+# 													owner=frappe.session.user,
+# 													territories_clause= territories_clause
+# 												), as_dict=True)
+			
+# 			#Get Open Leads owned by 
+# 			open_leads_prelim = frappe.get_all("Lead", filters=[
+# 					["contact_by", "=", ""], 
+# 					["awfis_lead_territory", "in", allowed_territories],
+# 					["owner", "=", frappe.session.user]
+# 					], fields=["*"])
+# 			open_leads = [ol for ol in open_leads_prelim if not(ol._assign) or len(ast.literal_eval(ol._assign)) == 1]
+# 			open_general = open_leads
+
+# 			other = frappe.get_all("Lead", filters=[["awfis_lead_territory", "in", allowed_territories], ["owner", "=", frappe.session.user]], fields=["*"], order_by="lead_state, contact_date DESC", limit=limit)	
+# 		else:
+# 			pass
+
+
+
+	
+	
+# 	# else:
+
+		
+# 	# 	# rsms_for_territories = frappe.get_all("DefaultValue", fields=["parent"], filters={"defkey": "Territory", "defvalue": lead.awfis_lead_territory, "parenttype": "User Permission"})
+
+		
+# 	# 		#Will fetch Leads for all people in territories.
+# 	# 		sql = """SELECT * FROM tabLead WHERE date(contact_date) = curdate() 
+# 	# 				{territories_clause} ORDER BY name DESC LIMIT {limit}"""
+# 	# 				.format(limit=limit,territories_clause= territories_clause)
+
+			
+# 	out = frappe._dict({
+# 		"follow_up_today": follow_up_today,
+# 		"open_general": open_general,
+# 		"other": other
+# 	})
+
+# 	return out
 
 
 @frappe.whitelist()
--- awfis_erpnext/awfis_erpnext/page/awfis_lead_list/awfis_lead_list.js
+++ awfis_erpnext/awfis_erpnext/page/awfis_lead_list/awfis_lead_list.js
@@ -14,7 +14,44 @@ frappe.pages['awfis_lead_list'].on_page_load = function(wrapper) {
 				console.log("Return", r.message);
 				var leads_html = frappe.render_template("awfis_lead_list", {"lead_data": r.message});
 				content.html(leads_html);
+				//content.find(".").prettyDate(this.attr('data-date'))
 			}
 		}
 	})
 }
+
+// frappe.views.AwfisLeadDocListView = frappe.views.DocListView.extend({
+// 	init_list: function(auto_run) {
+// 		var me = this;
+// 		// init list
+// 		this.make({
+// 			method: 'frappe.desk.reportview.get',
+// 			save_list_settings: true,
+// 			get_args: this.get_args,
+// 			parent: this.wrapper,
+// 			freeze: true,
+// 			start: 0,
+// 			page_length: this.page_length,
+// 			show_filters: true,
+// 			show_grid: true,
+// 			new_doctype: this.doctype,
+// 			allow_delete: this.allow_delete,
+// 			no_result_message: this.make_no_result(),
+// 			custom_new_doc: me.listview.make_new_doc || undefined,
+// 		});
+
+// 		// make_new_doc can be overridden so that default values can be prefilled
+// 		// for example - communication list in customer
+// 		if(this.listview.settings.list_view_doc) {
+// 			this.listview.settings.list_view_doc(this);
+// 		}
+// 		else{
+// 			$(this.wrapper).on("click", 'button[list_view_doc="'+me.doctype+'"]', function(){
+// 				(me.listview.make_new_doc || me.make_new_doc).apply(me, [me.doctype]);
+// 			});
+// 		}
+
+// 		if((auto_run !== false) && (auto_run !== 0))
+// 			this.refresh();
+// 	}
+// })
