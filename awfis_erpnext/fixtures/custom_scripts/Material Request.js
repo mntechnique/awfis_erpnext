3b78bc2e6824b663652631672fe4875fb9e355f4
Tanay kanhere <tanaykanhere@gmail.com>
2017-06-09 13:37:57 +0530

    Material Request Custom Script removed

--- awfis_erpnext/fixtures/custom_scripts/Material Request.js
+++ awfis_erpnext/fixtures/custom_scripts/Material Request.js
@@ -1,10 +1,10 @@
 //Allow only material transfers when user has just Stock User in roles.
 //Length == 3: user_roles=["Stock User", "All", "Guest"]
-frappe.ui.form.on('Material Request', 'validate', function(frm){
-	if ((roles.length == 3) && (roles[0] == "Stock User")) {
-		if (cur_frm.doc.material_request_type != "Material Transfer") {
-			validated=false;
-			frappe.msgprint('Not allowed.');
-		}
-	}
-});
+// frappe.ui.form.on('Material Request', 'validate', function(frm){
+// 	if ((roles.length == 3) && (roles[0] == "Stock User")) {
+// 		if (cur_frm.doc.material_request_type != "Material Transfer") {
+// 			validated=false;
+// 			frappe.msgprint('Not allowed.');
+// 		}
+// 	}
+// });
